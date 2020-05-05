657acef35a0c5ba30354bbaebc1374c8c6670662
dk649 <58308212+dk649@users.noreply.github.com>
2020-05-05 13:33:12 +0100

    Updates

---
+++ precache-manifest.9647f034d03d372b51ea8ebca921d910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e911a07894a3ab9e5ecc41384e10401",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "33a6e67504927d39af01",
+    "url": "/tic-tac-toe/static/css/main.63873d64.chunk.css"
+  },
+  {
+    "revision": "6c84d17184dc19a8d6af",
+    "url": "/tic-tac-toe/static/js/2.fe3becc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.fe3becc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a6e67504927d39af01",
+    "url": "/tic-tac-toe/static/js/main.ef393d71.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
